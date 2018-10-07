d91eadd7484446ab81f3d4a57e67f9ee04c69eb7
Winnie <winnie@winnies-air.lan>
2018-10-07 12:14:51 -0700

    changed the logic to check unique characters by not using additional data structures :rabbit:

--- src/ch01_is_unique.py
+++ src/ch01_is_unique.py
@@ -1,14 +1,11 @@
 def is_unique(input_str):
-    # keep the character that was iterated
-    char_collection = []
     # if a user input empty string
     if not input_str:
         return False
     else:
         for char in input_str:
-            if char not in char_collection:
-                char_collection.append(char)
-            else:
+            count = input_str.count(char)
+            if count > 1:
                 return False
         # after iterate through all the characters in the string
         else:
